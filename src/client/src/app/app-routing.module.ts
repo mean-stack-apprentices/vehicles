0d4cfbdce21ea53b9a5457891a812120a9d52c9f
Aman <amandeepk221@gmail.com>
2022-01-22 18:27:52 -0800

    feat(client): add routes for vehicle-input component

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { VehicleInputComponent } from './components/vehicle-input/vehicle-input.component';
 import { PageUsersComponent } from './pages/page-users/page-users.component';
 
 const routes: Routes = [
-  {path: 'users', component: PageUsersComponent}
+  {path: 'users', component: PageUsersComponent},
+  {path: 'vehicles', component: VehicleInputComponent}
+
 ];
 
 @NgModule({
