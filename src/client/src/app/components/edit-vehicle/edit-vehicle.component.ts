5b7349674bce84ba4569bdf3b97ee124b82b5690
Aman <amandeepk221@gmail.com>
2022-01-28 14:44:47 -0800

    feat(client): update vehicle

--- src/client/src/app/components/edit-vehicle/edit-vehicle.component.ts
+++ src/client/src/app/components/edit-vehicle/edit-vehicle.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { VehicleService } from 'src/app/services/vehicle.service';
 import { AppState } from 'src/app/store';
+import { updateVehicle } from 'src/app/store/actions/vehicle/vehicle.actions';
 import { vehicleSelector } from 'src/app/store/selectors/vehicle/vehicle.selectors';
 import { Vehicle } from '../../../../../shared/models/vehicle.model';
 
@@ -11,15 +14,36 @@ import { Vehicle } from '../../../../../shared/models/vehicle.model';
   styleUrls: ['./edit-vehicle.component.scss']
 })
 export class EditVehicleComponent implements OnInit {
-  selectedVehicle$ : Observable<Vehicle | null>
+  selectedVehicle : Vehicle | null = null;
+  editVehicleForm: FormGroup;
   
   constructor(
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private fb: FormBuilder,
+    private vehicleServices: VehicleService
+
   ) { 
-    this.selectedVehicle$ = this.store.select(vehicleSelector)
+    this.store.select(vehicleSelector).subscribe(data => this.selectedVehicle = data);
+
+    this.editVehicleForm = this.fb.group({
+      year: ['', Validators.required],
+      make: ['', Validators.required],
+      model: ['', Validators.required],
+    })
   }
 
   ngOnInit(): void {
+      this.editVehicleForm.get('year')?.setValue(this.selectedVehicle?.year);
+      this.editVehicleForm.get('make')?.setValue(this.selectedVehicle?.make);
+      this.editVehicleForm.get('model')?.setValue(this.selectedVehicle?.model);
+  }
+
+  editVehicle() {
+    
+    this.store.dispatch(updateVehicle({data: this.editVehicleForm.value, id: this.selectedVehicle?._id}));
+
+    this.editVehicleForm.reset();
+
   }
 
 }
