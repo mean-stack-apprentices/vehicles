c026b38f7de7362a6125f4d30aa2827f3004ded3
Aman <amandeepk221@gmail.com>
2022-01-28 13:17:50 -0800

    feat(client,store): create effects on load vehicles

--- src/client/src/app/store/effects/vehicle/vehicle.effects.ts
+++ src/client/src/app/store/effects/vehicle/vehicle.effects.ts
@@ -1,13 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { VehicleService } from 'src/app/services/vehicle.service';
+import { loadVehicles, loadVehiclesFailure, loadVehiclesSuccess } from '../../actions/vehicle/vehicle.actions';
 
 
 
 @Injectable()
 export class VehicleEffects {
+  loadVehicles$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadVehicles),
+      mergeMap(() =>
+        this.vehicleService.getVehicles().pipe(
+          map((data) => loadVehiclesSuccess( {data} )),
+          catchError((error) => of(loadVehiclesFailure({ error })))
+        )
+      )
+    )
+  );
 
 
-
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions,
+    private vehicleService: VehicleService
+  ) 
+  {}
 
 }
+
+
+
+
