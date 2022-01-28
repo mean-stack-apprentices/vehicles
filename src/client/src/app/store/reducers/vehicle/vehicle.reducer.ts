eeca025c56b8cf8c32893991702778cc9d7708dd
Aman <amandeepk221@gmail.com>
2022-01-28 12:07:05 -0800

    feat(client,store): create property selectedVehicle

--- src/client/src/app/store/reducers/vehicle/vehicle.reducer.ts
+++ src/client/src/app/store/reducers/vehicle/vehicle.reducer.ts
@@ -1,19 +1,23 @@
-import { Action, createReducer, on } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { Vehicle } from '../../../../../../shared/models/vehicle.model';
+import { selectVehicle } from '../../actions/vehicle/vehicle.actions';
 
 
 export const vehicleFeatureKey = 'vehicle';
 
 export interface State {
-
+  selectedVehicle: Vehicle | null;
 }
 
 export const initialState: State = {
-
+  selectedVehicle: null
 };
 
 
 export const reducer = createReducer(
   initialState,
-
+  on(selectVehicle, (state, action) => {
+    return {...state, selectedVehicle: action.data}
+  })
 );
 
