5ebb589fb13691c9f9b3f529c484d2c8df67fb29
Aman <amandeepk221@gmail.com>
2022-01-28 13:29:53 -0800

    feat(client): add routes

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditVehicleComponent } from './components/edit-vehicle/edit-vehicle.component';
 import { VehicleInputComponent } from './components/vehicle-input/vehicle-input.component';
 
 const routes: Routes = [
-  {path: 'vehicles', component: VehicleInputComponent}
+  {path: '', component: VehicleInputComponent},
+  {path: 'edit-vehicle', component: EditVehicleComponent}
 ];
 
 @NgModule({
