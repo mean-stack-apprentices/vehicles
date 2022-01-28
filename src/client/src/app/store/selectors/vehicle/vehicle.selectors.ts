2a03cd33bea205ebbedd6cba7409c9d6939cf5ac
Aman <amandeepk221@gmail.com>
2022-01-28 12:13:58 -0800

    feat(client,store): create vehicleSelector to assign selected value

--- src/client/src/app/store/selectors/vehicle/vehicle.selectors.ts
+++ src/client/src/app/store/selectors/vehicle/vehicle.selectors.ts
@@ -1,2 +1,12 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState } from '../..';
+import * as fromVehicle from '../../reducers/vehicle/vehicle.reducer';
+
+const vehicleFeatureSelector = createFeatureSelector<AppState, fromVehicle.State>(fromVehicle.vehicleFeatureKey);
+
+export const vehicleSelector = createSelector(
+  vehicleFeatureSelector,
+  (state) => state.selectedVehicle
+);
+
 
