3b50d2bb4629df8687c014bd43fe1381d0199d3b
Aman <amandeepk221@gmail.com>
2022-02-03 06:42:57 -0800

    feat(server, schemas): create imgUrl property for schema

--- src/server/schemas/vehicle.schema.ts
+++ src/server/schemas/vehicle.schema.ts
@@ -7,7 +7,8 @@ const {model, Schema} = mongoose;
 const vehicleSchema = new Schema<Vehicle>({
     year: {type: Number, required: true},
     make: {type: String, required: true},
-    model: {type: String, required: true}
+    model: {type: String, required: true},
+    imgUrl: {type: String}
 });
 
 export const VehicleModel = model<Vehicle>('Vehicle',vehicleSchema);
